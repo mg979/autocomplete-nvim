12fb39309f4698f14affdd5ff155d255b8959551
mg979 <mg1979.git@gmail.com>
2020-07-04 08:11:00 +0200

    fix matching.lua

--- lua/autocomplete/matching.lua
+++ lua/autocomplete/matching.lua
@@ -2,25 +2,28 @@ local vim = vim
 local util = require'autocomplete.util'
 local M = {}
 
+local function set_case(prefix, word)
+  if vim.g.autocomplete.ignore_case == 1 then
+    prefix = string.lower(prefix)
+    word = string.lower(word)
+  end
+  return prefix, word
+end
+
 local function fuzzy_match(prefix, word)
+  prefix, word = set_case(prefix, word)
   local score = util.fuzzy_score(prefix, word)
   return score < 1, score
 end
 
 local function substring_match(prefix, word)
+  prefix, word = set_case(prefix, word)
   return string.find(word, prefix)
 end
 
 local function exact_match(prefix, word)
+  prefix, word = set_case(prefix, word)
   return vim.startswith(word, prefix)
-end
-
-local function adjusted_case(prefix, word)
-  if vim.g.autocomplete.ignore_case then
-    prefix = string.lower(prefix)
-    word = string.lower(word)
-  end
-  return prefix, word
 end
 
 local matching_strategy = {
@@ -32,7 +35,6 @@ local matching_strategy = {
 function M.matching (sources, prefix, item)
   local matcher_list = vim.b.completion_matching or vim.g.autocomplete.matching
   local matching_priority = 2
-  prefix, item.word = adjusted_case(prefix, item.word)
   for _, method in ipairs(matcher_list) do
     local is_match, score = matching_strategy[method](prefix, item.word)
     if is_match then
